2e62b3f15c38bf10408953443d7da63d64c33575
Doloment <doloment@internet.ru>
2025-03-14 21:35:41 +0700

    Chages for Ensure; Add planned mod folders; Move utf8_api to Utf8

--- mods/API/Ensure/init.lua
+++ mods/API/Ensure/init.lua
@@ -9,18 +9,34 @@ Ensure = {}
 --- @param function_name  string  Name of the parent function for error reporting
 function Ensure.argType(arg, expectedType, n, function_name)
 	local actualType = type(arg)
-	assert(actualType == expectedType, 'bad argument #%s to \'%s\' (%s expected, got %s)', tostring(n), function_name, expectedType, actualType)
+	assert(actualType == expectedType, string.format(
+		'bad argument #%s to \'%s\' (%s expected, got %s)',
+		tostring(n),
+		function_name,
+		expectedType,
+		actualType))
 end
 
---- @param arg           any     The argument value to type-check
---- @param n             number  Argument position in function call (1-based index)
+local ensureArgType = Ensure.argType
+
+--- @param arg            string  The string to check
+--- @param n              number  Argument position in function call (1-based index)
+--- @param function_name  string  Name of the parent function for error reporting
+function Ensure.stringArgNotEmpty(arg, n, function_name)
+	ensureArgType(arg, 'string', 1, 'Ensure.stringArgNotEmpty')
+	assert(#arg ~= 0, string.format('bad argument #%s to \'%s\' (string cannot be empty)', n,  function_name))
+end
+
+--- @param arg            any     The argument value to check
+--- @param n              number  Argument position in function call (1-based index)
 --- @param function_name  string  Name of the parent function for error reporting
 function Ensure.argNotNil(arg, n, function_name)
-	assert(arg ~= nil, 'bad argument #%s to \'%s\' (key cannot be nil)', n,  function_name)
+	assert(arg ~= nil, string.format('bad argument #%s to \'%s\' (key cannot be nil)', n,  function_name))
 end
 
-
+--- @param divider        number  The number to check
+--- @param function_name  string  Name of the parent function for error reporting
 function Ensure.zeroDivision(divider, function_name)
-	assert(divider ~= 0, 'Division by zero in \'%s\'', function_name)
+	assert(divider ~= 0, string.format('Division by zero in \'%s\'', function_name))
 end
 

4f30c20ebfa771a9b93d9d8c889e712dbd89e87d
Vanic <vanicgame@yandex.ru>
2025-09-03 22:09:37 +0000

    Node: Add VisualEnum. Write Node:new() logic

--- mods/CORE/Node/src/Node.lua
+++ mods/CORE/Node/src/Node.lua
@@ -1,15 +1,18 @@
+---@type Item.VisualEnum
+local VisualEnum = Core.Item.require('Item.VisualEnum')
+
 ---@class Node : Item
 local Node = Core.Item:getModClassExtended({
 	--TODO: Че тут писать?
 })
 
-
+---@param itemDef Item.ItemDefinition
 ---@return Node
-function Node:new()
-	---@type Node
-	local instance
+function Node:new(itemDef)
+	itemDef.settings.visual = VisualEnum.ITEM_3D
 
-	--TODO: Это надо вообще?
+	---@type Node
+	local instance = Core.Item:getModClassInstance(itemDef)
 
 	return instance
 end

6fadcf1c8db040960a255506f316a2da15ea2fc7
Vanic <vanicgame@yandex.ru>
2025-09-22 17:58:32 +0000

    Node: add getFactory & getExtended methods, add support defaultItemDef

--- mods/CORE/Node/src/Node.lua
+++ mods/CORE/Node/src/Node.lua
@@ -2,6 +2,7 @@
 local VisualEnum = Core.Item.require('Item.VisualEnum')
 
 ---@class Node : Item
+---@field defaultDef  Node.NodeDefinition
 local Node = Core.Item:getModClassExtended({})
 
 ---@param nodeDef Node.NodeDefinition
@@ -11,6 +12,33 @@ function Node:new(nodeDef)
 
 	---@type Node
 	local instance = Core.Item:getModClassInstance(nodeDef)
+
+	return instance
+end
+
+---Allows you to create a child class of an node by specifying
+---default parameters that will be added to the node's parameters
+---when an instance is received.
+---@param defaultDef  Node.NodeDefinition
+---@return Node
+function Node:getExtended(defaultDef)
+	---@type Node
+	local ChildClass = Mod:getClassExtended(self, {
+		defaultNodeDef = defaultDef,
+	})
+
+	return ChildClass
+end
+
+---Returns an instance of the `Factory` class that can be used to mass-register identical items.
+---@param defaultDef  Node.NodeDefinition
+---@return                Factory
+function Node:getFactory(defaultDef)
+	if defaultDef == nil then
+		defaultDef = self.defaultDef
+	end
+
+	local instance = Core.Factory():new(self, defaultDef)
 
 	return instance
 end

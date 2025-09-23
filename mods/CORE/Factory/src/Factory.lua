51780794a3b795d00dd689b068ff2f79296a568f
Vanic <vanicgame@yandex.ru>
2025-09-23 18:39:28 +0000

    Factory: add new method registerNodesByShortDef and minor fixes

--- mods/CORE/Factory/src/Factory.lua
+++ mods/CORE/Factory/src/Factory.lua
@@ -10,7 +10,7 @@ function Factory:new(itemClass, defaultDef)
 	---@type Factory
 	local instance = setmetatable({
 		defaultDef = defaultDef,
-		itemClass      = itemClass,
+		itemClass  = itemClass,
 	}, {__index = self})
 
 	return instance
@@ -22,13 +22,52 @@ local defMerge = function(...)
 	return table.merge(...)
 end
 
----@param itemDefs  Item.ItemDefinition[] | Node.NodeDefinition[]  List of nodes with parameters unique to them.
-function Factory:registerItems(itemDefs)
-	---@param nodeDef  Item.ItemDefinition | Node.NodeDefinition
-	for _, nodeDef in ipairs(itemDefs) do
+---@param itemDefs    Item.ItemDefinition[] | Node.NodeDefinition[]  List of nodes with parameters unique to them.
+---@param addModName  boolean  Whether to add the mod name to the settings.name. Default: `true`
+function Factory:registerItems(itemDefs, addModName)
+	if addModName == nil then
+		addModName = true
+	end
 
-		---@type Item.ItemDefinition
-		local nodeDef = defMerge(nodeDef, self.defaultDef, true)
+	local modName = Mod.getInfo().shortName
+
+	---@param nodeDef  Item.ItemDefinition | Node.NodeDefinition
+	for _, itemDef in ipairs(itemDefs) do
+		itemDef = defMerge(itemDef, self.defaultDef, true)
+
+		if addModName then
+			itemDef.settings.name = modName .. ':' .. itemDef.settings.name
+		end
+
+		self.itemClass:new(itemDef)
+
+	end
+end
+
+---@param shortNodeDefs  table  Table of the form `{ {name, title, description, tiles},  {name, title, description, tiles}, ...}`
+---@param addModName     boolean?  Whether to add the mod name to the settings.name. Default: `true`
+function Factory:registerNodesByShortDef(shortNodeDefs, addModName)
+	if addModName == nil then
+		addModName = true
+	end
+
+	local modName = Mod.getInfo().shortName
+
+	for _, shortNodeDef in ipairs(shortNodeDefs) do
+		---@type Node.NodeDefinition
+		local nodeDef = {
+			settings = {
+				name = shortNodeDef[1],
+				title = shortNodeDef[2],
+				description = shortNodeDef[3],
+				tiles = shortNodeDef[4]
+			}
+		}
+		nodeDef = defMerge(nodeDef, self.defaultDef, true)
+
+		if addModName then
+			nodeDef.settings.name = modName .. ':' .. nodeDef.settings.name
+		end
 
 		self.itemClass:new(nodeDef)
 

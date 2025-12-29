40c1d34124c856dae04e47cfd4a18eb6764add1a
Vanic <vanicgame@yandex.ru>
2025-12-29 14:50:54 +0300

    ApiLoader: add GlobalStep.lua

--- mods/API/ApiLoader/src/Api.lua
+++ mods/API/ApiLoader/src/Api.lua
@@ -1,4 +1,6 @@
+Api = {}
+
 local modInfo = Mod.getInfo('smc__api__api_loader')
 local require = modInfo.require
 
-require('Api')
+require('GlobalStep')
---
+++ mods/API/ApiLoader/src/Api/GlobalStep.lua
@@ -0,0 +1,60 @@
+---@type {timeDelay:number, callbackFunc:fun(deltaTime:number), timer:number}[]
+local callsWithDelay = {}
+
+---@param timeDelay     number
+---@param callbackFunc  fun(deltaTime)
+function Api.registerGlobalStepWithDelay(timeDelay, callbackFunc)
+	table.insert(callsWithDelay, {
+		timeDelay    = timeDelay,
+		callbackFunc = callbackFunc,
+		timer        = 0
+	})
+end
+
+core.register_globalstep(function(deltaTime)
+	---@type number
+	local timer
+
+	for _, callback in ipairs(callsWithDelay) do
+		timer = callback.timer
+		timer = timer + deltaTime
+
+		if timer >= callback.timeDelay then
+			callback.callbackFunc(deltaTime)
+			callback.timer = 0
+		else
+			callback.timer = timer
+		end
+	end
+end)
+
+---@type {timeDelay:number, callbackFunc:fun(deltaTime:number, player), timer:number}[]
+local callsForEachPlayer = {}
+
+---@param timeDelay     number
+---@param callbackFunc  fun(player, deltaTime)
+function Api.registerGlobalStepForEachPlayer(timeDelay, callbackFunc)
+	table.insert(callsWithDelay, {
+		timeDelay    = timeDelay,
+		callbackFunc = callbackFunc,
+		timer        = 0
+	})
+end
+
+core.register_globalstep(function(deltaTime)
+	---@type number
+	local timer
+	for _, player in ipairs(core.get_connected_players()) do
+		for _, callback in ipairs(callsForEachPlayer) do
+			timer = callback.timer
+			timer = timer + deltaTime
+
+			if timer >= callback.timeDelay then
+				callback.callbackFunc(deltaTime, player)
+				callback.timer = 0
+			else
+				callback.timer = timer
+			end
+		end
+	end
+end)

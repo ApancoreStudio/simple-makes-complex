efeb5c75cd0eaa326fb9aae5cb72a8fd5d1e85cc
Vanic <vanicgame@yandex.ru>
2025-12-30 19:10:46 +0300

    Add draft plants

--- mods/GAME/ITEMS/plants/init.lua
+++ mods/GAME/ITEMS/plants/init.lua
@@ -1,5 +1,8 @@
 local modInfo = Mod.getInfo('smc__core__plant')
 
+---@type TileSheet
+local TileSheet = Mod.getInfo('smc__core__tilesheet').require('TileSheet')
+
 ---@type  Plant
 local Plant = modInfo.require('Plant')
 
@@ -11,3 +14,17 @@ Plant:new({
 		place_param2 = 3,
 	},
 })
+
+local ts = TileSheet:new('plants_test_sheet.png', 16, 16)
+
+
+for i=0, 3 do
+	Plant:new({
+		settings = {
+			name = 'plants:plant'..tostring(i),
+			description = 'Plant',
+			tiles = { ts:t(i,0) },
+			place_param2 = 3,
+		},
+	})
+end

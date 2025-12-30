4e823bfee63288a92eb96407de97f7da333679f0
Vanic <vanicgame@yandex.ru>
2025-12-30 18:13:11 +0300

    Math: add annotation to function

--- mods/API/Math/init.lua
+++ mods/API/Math/init.lua
@@ -1,9 +1,19 @@
 local math = math
 
-math.lerp = function (a, b, t)
-	return a + (b - a) * t
-end
-
+---Limiting the value by minimum and maximum value.
+---@param value  number  The value to be limited.
+---@param min    number  Minimum limit.
+---@param max    number  Maximum limit.
 math.clamp = function(value, min, max)
 	return math.min(math.max(value, min), max)
+end
+
+---Linear interpolation.
+---@param a  number  Minimum value.
+---@param b  number  Maximum value.
+---@param t  number  Number in range [0, 1].
+math.lerp = function (a, b, t)
+	t = math.clamp(t, 0, 1)
+
+	return a + (b - a) * t
 end
